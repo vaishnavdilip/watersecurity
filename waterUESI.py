dc5d137a9f6fc98780603dc94fed693d7db03971
vaishnavdilip <vaishnav.programming@gmail.com>
2022-06-07 01:40:03 +0200

    modified requirements file

--- waterUESI.py
+++ waterUESI.py
@@ -2,7 +2,7 @@
 import pandas as pd
 import numpy as np
 
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
 
 from sklearn.decomposition import PCA
 from sklearn.neighbors import kneighbors_graph 
