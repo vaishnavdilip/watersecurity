f7eecfcd7da2ef6abcf1139427f9b52ca390a48f
vaishnavdilip <vaishnav.programming@gmail.com>
2022-06-07 01:28:13 +0200

    Added unsupervised

--- app.py
+++ app.py
@@ -10,17 +10,15 @@ from plotly.subplots import make_subplots
 
 import plotly.graph_objects as go
 import numpy as np
-import pandas as pd
 
-from graphs import make_plot
+from waterUESI import cluster_neighbors, cluster_waterStress, cities_df
 
-
-app = dash.Dash(__name__,title='MDA Class 2022',external_stylesheets=[dbc.themes.BOOTSTRAP],serve_locally = False)
+app = dash.Dash(__name__,title='Water Security',external_stylesheets=[dbc.themes.BOOTSTRAP],serve_locally = False)
 
 # add this for heroku
 server = app.server
 
-# Chart
+#Chart
 fig = make_subplots(rows=1, cols=1)
 fig.add_trace(
     go.Scatter(x=np.arange(0,10,1),
@@ -29,39 +27,27 @@ fig.add_trace(
     row=1, col=1)
 fig.update_layout(width=1500)
 
-# Currency Options
+# City Options
 dropdown = dcc.Dropdown(
-    id='id_currency',
-    options=[{"label":'CHF','value':'CHF'},
-             {"label":'GBP','value':'GBP'},
-             {"label":'SEK','value':'SEK'},
-             ],
-    value='CHF')
+        id='id_cities',
+        options=[
+            {'label':i, 'value':i} for i in cities_df['c'].unique()
+        ],
+    value='bogota')
 
 # Start Date,  End Date & Number of Mixtures
-input_groups = dbc.Row(dbc.Col(
-    html.Div([
-    dbc.InputGroup([
-        dbc.Input(id='id_start_date',value="2020-01-01", type="text")],),
-    dbc.InputGroup([
-        dbc.Input(id='id_end_date',value="2021-01-01",type="text")]),
-    dbc.InputGroup([
-        dbc.Input(id='id_nbr_mixtures',value=3,type='number')]),
-
-    dropdown]
-)))
+input_groups = dbc.Row(dbc.Col(html.Div(dropdown)))
 
 app.layout = dbc.Container(
     [
-        html.Div(children=[html.H1(children='Gaussian Mixtures'),
-                           html.H2(children='Data Source: ECB'),
-                           html.H4(children='...',id='id_title')],
+        html.Div(children=[html.H1(children='Water Security')],
                  style={'textAlign':'center','color':'black'}),
         html.Hr(),
         dbc.Row(
             [
                 dbc.Col(input_groups, md=2),
-                dbc.Col(dcc.Graph(id="id_graph",figure=fig), md=10),
+                dbc.Col(dcc.Graph(id="id_scatter",figure=fig), md=10),
+                dbc.Col(dcc.Graph(id="id_density",figure=fig), md=10),
             ],
             align="center",
         ),
@@ -70,43 +56,20 @@ app.layout = dbc.Container(
 )
 
 @app.callback(
-    Output('id_title','children'),
-    Output('id_graph','figure'),
-    [Input('id_currency', 'value'),
-     Input('id_start_date','value'),
-     Input('id_end_date','value'),
-     Input('id_nbr_mixtures','value')
+    Output('id_scatter','figure'),
+    Output('id_density','figure'),
+    [
+     Input('id_cities','value')
      ]
 )
-def update_chart(currency_value,start_date,end_date,nbr):
-    try:
-        start_date = pd.to_datetime(start_date)
-        end_date = pd.to_datetime(end_date)
-    except:
-        fig = make_subplots(rows=1, cols=1)
-        fig.add_trace(
-            go.Scatter(x=np.arange(0,10,1),
-                       y=np.arange(0,10,1)*2 + np.random.randn(),
-                       name='Example'),
-            row=1, col=1)
-        fig.update_layout(width=1500)
-        return 'Error in Input' , fig
-
-
-    if nbr is None:
-        nbr = 3
-
-    nbr = max(1,min(5,nbr))
+def update_chart(city):
+ 
     params = {}
-    params['start_date'] = start_date
-    params['end_date'] = end_date
-    params['currency'] = currency_value
-    params['nbr_mixtures'] = nbr
-    params['periods'] = 1
-    fig = make_plot(params)
+    params['city']=city
+    scatter = cluster_neighbors(params)
+    density = cluster_waterStress(params)
 
-    return 'Gaussian Mixtures for EUR' + currency_value + \
-           ' (' + str(nbr) + ' Mixtures) ' + start_date.strftime('%Y-%m-%d') + '->' + end_date.strftime('%Y-%m-%d') , fig
+    return scatter,density
 
 
 if __name__ == '__main__':
